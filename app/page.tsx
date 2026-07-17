a9be1d229961aee193edf1b71275a2493b96f213
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-17 23:30:03 +0200

    Admin lesson editor: update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import "./search-bar.css";
 import SchoolLearningPortal from "./SchoolLearningPortal";
+import { isCurrentUserAdmin } from "@/lib/admin/server";
 
-export default function HomePage() {
-  return <SchoolLearningPortal />;
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+  const isAdmin = await isCurrentUserAdmin();
+  return <SchoolLearningPortal isAdmin={isAdmin} />;
 }
