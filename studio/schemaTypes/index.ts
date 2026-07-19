fbbac5dc42eff5db016e4e1c4a4f1e08ac8c31c6
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-19 14:09:29 +0200

    Register standalone Studio schema types

---
+++ studio/schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import {chapter} from './chapter'
+import {grade} from './grade'
+import {lesson} from './lesson'
+import {lessonFlashcard} from './lesson-flashcard'
+import {subject} from './subject'
+
+export const schemaTypes = [grade, subject, chapter, lessonFlashcard, lesson]
