d5621f1b0945748992de05afb41f85df86b528ac
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-19 15:17:54 +0200

    Register lesson table schema types

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -2,6 +2,17 @@ import {chapter} from './chapter'
 import {grade} from './grade'
 import {lesson} from './lesson'
 import {lessonFlashcard} from './lesson-flashcard'
+import {lessonTable, lessonTableCell, lessonTableColumn, lessonTableRow} from './lesson-table'
 import {subject} from './subject'
 
-export const schemaTypes = [grade, subject, chapter, lessonFlashcard, lesson]
+export const schemaTypes = [
+  grade,
+  subject,
+  chapter,
+  lessonFlashcard,
+  lessonTableColumn,
+  lessonTableCell,
+  lessonTableRow,
+  lessonTable,
+  lesson,
+]
