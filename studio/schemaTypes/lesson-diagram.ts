9bdae604e47bb104258c098c7c0e3140f865ec3b
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-09-12 15:18:14 +0200

    Add fluid dynamics lesson diagram types

--- studio/schemaTypes/lesson-diagram.ts
+++ studio/schemaTypes/lesson-diagram.ts
@@ -28,6 +28,8 @@ export const lessonDiagram = defineType({
           {title: 'Kapilariteti - lagia dhe moslagia', value: 'capillaryWetting'},
           {title: 'Kapilariteti - elevacioni dhe depresioni', value: 'capillaryLevels'},
           {title: 'Kapilariteti - baraspesha e forcave', value: 'capillaryBalance'},
+          {title: 'Dinamika e fluideve - vijat e rrymimit', value: 'fluidStreamlines'},
+          {title: 'Dinamika e fluideve - ekuacioni i kontinuitetit', value: 'continuityTube'},
         ],
       },
       validation: (rule) => rule.required(),
